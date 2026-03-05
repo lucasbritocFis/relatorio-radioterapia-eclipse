1a69590f53651da1565c6c2063346fa9c0d77127
lucasbritocFis <lucasdbcavalcanti@gmail.com>
2026-03-05 09:34:17 -0300

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-Coloque aqui os seguintes arquivos:
-
-- logo_rede_americas.png
-- faixa_degrade_roxo.png
-- ass_lucas.jpg           (renomear de "ass Lucas carimbada.jpg")
-- ass_guilherme.jpg       (renomear de "ass Guilherme carimbada.jpg")
-- Modelo_RESUMO_ALTA_AMERICAS.pdf
